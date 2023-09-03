30b242b8d814da8ba7a56eabad757172aba5163e
cjwoo96 <141621371+cjwoo96@users.noreply.github.com>
2023-09-03 15:55:24 +0900

    [level 3] Title: 양과 늑대, Time: 1.06 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+class Solution {
+ 
+	static int answer = Integer.MIN_VALUE;
+	static ArrayList<Integer>[] arr;
+
+	static void DFS(ArrayList<Integer> list, int sheep, int wolf, int nextnode,int[] info, int[][] edges) {
+		if (info[nextnode] == 0)
+			sheep++;
+		else
+			wolf++;
+        answer = Math.max(answer, sheep);
+		if (sheep == wolf || list.isEmpty()) {
+			
+			return;
+		}
+
+		ArrayList<Integer> list2 = new ArrayList<Integer>();
+
+		for (int i = 0; i < list.size(); i++) {
+			list2.add(list.get(i));
+		}
+		list2.remove(Integer.valueOf(nextnode));
+
+		for (int i = 0; i < arr[nextnode].size(); i++) {
+			list2.add(arr[nextnode].get(i));
+		}
+
+		for (int i = 0; i < list2.size(); i++) {
+			DFS(list2, sheep, wolf, list2.get(i),info,edges);
+		}
+
+	}
+    public int solution(int[] info, int[][] edges) {
+       arr = new ArrayList[info.length];
+		for (int i = 0; i < info.length; i++) {
+			arr[i] = new ArrayList<>();
+		}
+
+		for (int i = 0; i < edges.length; i++) {
+			arr[edges[i][0]].add(edges[i][1]);
+		}
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(0);
+		DFS(list, 0, 0, 0,info,edges);
+		return answer;
+    }
+}
