48551d2446f6f5666c61273af99603edafb5df96
cjwoo96 <141621371+cjwoo96@users.noreply.github.com>
2023-09-22 09:32:15 +0900

    [Silver I] Title: 단지번호붙이기, Time: 140 ms, Memory: 13184 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,63 +1,68 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
-class spot_2667{
-    int x,y;
-    public spot_2667(int x,int y){
-        this.x=x;
-        this.y=y;
-
-    }
-}
 public class Main {
-    static int n;
-    static int answer=0;
-    static int[][] arr;
-    static int[] dx={-1,0,1,0};
-    static int[] dy={0,-1,0,1};
-    static ArrayList<Integer> List =new ArrayList<>();
-    static Queue<spot_2667> Q=new LinkedList<>();
-    public static void DFS_2667(){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(arr[i][j]==1){
-                    Q.offer(new spot_2667(i,j));
-                    arr[i][j]=0;
-                    int len=1;
-                    answer++;
-                    while(!Q.isEmpty()){
-                        spot_2667 sp=Q.poll();
-                        for(int s=0;s<4;s++){
-                            int nx=dx[s]+sp.x;
-                            int ny=dy[s]+sp.y;
-                            if(nx<=n-1&&ny<=n-1&&nx>=0&&ny>=0&&arr[nx][ny]==1){
-                                Q.offer(new spot_2667(nx,ny));
-                                arr[nx][ny]=0;
-                                len++;
-                            }
-                        }
-                    }
-                    List.add(len);
-                }
-            }
-        }
-    }
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        n=sc.nextInt();
-        String str="";
-        arr=new int[n][n];
-        for(int i=0;i<n;i++){
-            str=sc.next();
-            for(int j=0;j<n;j++){
-                arr[i][j]=Integer.parseInt(str.substring(j,j+1));
-            }
-        }
-
-        DFS_2667();
-        System.out.println(answer);
-        Collections.sort(List);
-        for(int x:List){
-            System.out.println(x);
-        }
-    }
+	static int N;
+	static int[][] arr;
+	static boolean[][] visited;
+	static int cnt; // 각 단지 수 카운트
+	static List<Integer> val; // 단지 수 배열
+	
+	// 방향델타 (상 우 하 좌)
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N  = sc.nextInt();
+		arr = new int[N+2][N+2];
+		visited = new boolean[N+2][N+2];
+		val = new ArrayList<>();
+		
+		for (int i = 0; i < N; i++) {
+			String s = sc.next();
+			for (int j = 0; j < N; j++) {
+				arr[i+1][j+1] = Integer.parseInt(String.valueOf(s.charAt(j)));
+			}
+		} // 인풋 받기 끝
+		
+		// 시작점 배열 돌면서 찾는다
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (!visited[i][j] && arr[i][j] == 1) {
+					cnt++; // 시작 단지
+					dfs(i, j);
+					val.add(cnt); // 한 단지 개수 저장
+					cnt = 0; // 다음 단지 넘어갈 때 초기화
+				}
+			}
+		}
+		
+		System.out.println(val.size());
+		Collections.sort(val);
+		for (int i = 0; i < val.size(); i++) {
+			System.out.println(val.get(i));
+		}
+	}
+	
+	static void dfs(int x, int y) {
+		
+		visited[x][y] = true; // 방문처리
+		
+		// 사방 돌면서 탐색
+		for (int d= 0; d < 4; d++) {
+			int nr = x + dr[d];
+			int nc = y + dc[d];
+			if (arr[nr][nc] == 1 && !visited[nr][nc]) {
+				// 진출 가능
+				visited[nr][nc] = true;
+				cnt++;
+				dfs(nr, nc);
+			}
+		}
+	}
 }
