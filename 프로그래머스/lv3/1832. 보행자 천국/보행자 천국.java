7465cab705fe5a6fcb290281f6b759d6f92d7238
cjwoo96 <141621371+cjwoo96@users.noreply.github.com>
2023-10-09 14:18:54 +0900

    [level 3] Title: 보행자 천국, Time: 168.54 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/lv3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/lv3/1832. 보행자 천국/보행자 천국.java
@@ -1,6 +1,12 @@
 import java.util.PriorityQueue;
 
 class Solution {
+    int MOD = 20170805;
+    static long[][] Count;
+	static int[] dx = { 0, 1 };
+	static int[] dy = { 1, 0 };
+
+
 
 	static class point implements Comparable<point> {
 		int x, y;
@@ -13,6 +19,7 @@ class Solution {
 			this.len = len;
 		}
 
+		// 포인트의 거리가 작을수록 우선순위가 높음.
 		@Override
 		public int compareTo(point o) {
 			// TODO Auto-generated method stub
@@ -20,52 +27,40 @@ class Solution {
 		}
 
 	}
-    static int[] dx = { 0, 1 };
-	static int[] dy = { 1, 0 };
-    int MOD = 20170805;
     public int solution(int m, int n, int[][] cityMap) {
-        long[][] Count = new long[m][n];
+       Count = new long[m][n];
+		// 출발지는 1가지 이므로 1으로 초기화
 		Count[0][0] = 1;
-		PriorityQueue<point> Q = new PriorityQueue<>();
-		Q.add(new point(0, 0, 0));
-		while (!Q.isEmpty()) {
-			point p = Q.poll();
+		PriorityQueue<point> pq = new PriorityQueue<>();
+		// 시작점 넣어주기
+		pq.add(new point(0, 0, 0));
+		while (!pq.isEmpty()) {
+			point p = pq.poll();
+			// 오른쪽, 아래 방향 탐색
 			for (int i = 0; i < 2; i++) {
 				int nx = p.x + dx[i];
 				int ny = p.y + dy[i];
-				if (nx >= 0 && nx < m && ny >= 0 && ny < n && cityMap[nx][ny] != 1) {
-					if (cityMap[nx][ny] == 2) {
-						int cnt = 1;
-						while (nx >= 0 && nx < m && ny >= 0 && ny < n&&cityMap[nx][ny] == 2) {
-							cnt++;
-							nx += dx[i];
-							ny += dy[i];
-						}
-
-						if (nx >= 0 && nx < m && ny >= 0 && ny < n && cityMap[nx][ny] != 1) {
-							if (Count[nx][ny] == 0) {
-								Count[nx][ny] = Count[p.x][p.y];
-								Q.add(new point(nx, ny, p.len + cnt));
-							} else {
-								Count[nx][ny] += Count[p.x][p.y];
-							}
-                             Count[nx][ny]=Count[nx][ny] % MOD;
-						}
-					} else {
-						if (Count[nx][ny] == 0) {
-							Count[nx][ny] = Count[p.x][p.y];
-							Q.add(new point(nx, ny, p.len + 1));
-						} else {
-							Count[nx][ny] += Count[p.x][p.y];
-						}
-                         Count[nx][ny]=Count[nx][ny] % MOD;
+				// 다음 칸이 0 인 경우
+				if (nx >= 0 && nx < m && ny >= 0 && ny < n && cityMap[nx][ny] == 0) {
+					// 처음 방문인 경우 pq 에 넣어주고 값 저장.
+					if (Count[nx][ny] == 0)
+						pq.add(new point(nx, ny, nx + ny));
+					Count[nx][ny] += Count[p.x][p.y];
+					Count[nx][ny] %= MOD;
+				} else if (nx >= 0 && nx < m && ny >= 0 && ny < n && cityMap[nx][ny] == 2) {
+					while (nx >= 0 && nx < m && ny >= 0 && ny < n && cityMap[nx][ny] == 2) {
+						nx += dx[i];
+						ny += dy[i];
 					}
-
+					if (nx >= 0 && nx < m && ny >= 0 && ny < n && cityMap[nx][ny] == 0) {
+						if (Count[nx][ny] == 0)
+							pq.add(new point(nx, ny, nx + ny));
+						Count[nx][ny] += Count[p.x][p.y];
+						Count[nx][ny] %= MOD;
+					}
 				}
-               
-
 			}
 		}
-		return (int)(Count[m - 1][n - 1]);
+		return (int)Count[m - 1][n - 1];
     }
 }
